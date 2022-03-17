523ef2c1d32b0ac10607f291991b798a7f5ca98c
Haven Fricke <robslaven@live.com>
2022-03-17 11:20:36 -0600

    note push

--- planIt.client/src/components/Note.vue
+++ planIt.client/src/components/Note.vue
@@ -1,17 +1,55 @@
 <template>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque iusto
-    distinctio sequi culpa pariatur. Vero quo temporibus architecto saepe
-    eligendi fugit doloremque minus maiores consequuntur, vitae, quaerat
-    molestias voluptas quod.
-  </p>
+  <div class="row border shadow d-flex justify-content-end p-3 me-3">
+    <p class="col-12">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque iusto
+      distinctio sequi culpa pariatur. Vero quo temporibus architecto saepe
+      eligendi fugit doloremque minus maiores consequuntur, vitae, quaerat
+      molestias voluptas quod.
+    </p>
+    <i
+      @click="deleteNote"
+      class="col-2 fs-5 mdi mdi-delete text-end hoverable mx-3"
+    ></i>
+  </div>
 </template>
 
 <script>
+import { computed } from "@vue/reactivity"
+import { useRoute } from "vue-router"
+import { notesService } from "../services/NotesService"
+import { logger } from "../utils/Logger"
+import Pop from "../utils/Pop"
+import { AppState } from "../AppState"
 export default {
+  setup() {
+    const route = useRoute()
+    return {
+      async deleteNote() {
+        try {
+          await notesService.deleteNote(route.params.id, notes.id)
+          if (await Pop.confirm()) {
 
+          }
+        } catch (error) {
+          logger.error(error)
+          Pop.toast(error.message)
+        }
+      },
+      notes: computed(() => AppState.notes)
+    }
+  }
 }
 </script>
 
 <style>
+.hoverable:hover {
+  transform: scale(1.09);
+  filter: drop-shadow(0px 15px 10px rgba(0, 0, 0, 0.3));
+  transition: 50ms ease-in-out;
+  cursor: pointer;
+}
+.hoverable:active {
+  transform: scale(0.98);
+  transition: 50ms ease-in-out;
+}
 </style>
